16f17406aa596da930e1290c9e44d2f3af17763f
Benjamin Bradley <bbradle1@umd.edu>
2018-02-26 21:55:38 -0500

    Fixed problem where program wouldn't stop
    The program was originally set to run the query until it reaches the 10,001st result (the maximum number allowed by the API). The program now reads the results returned by the query to find the number of results returned, so it can use that as the number to stop running.

--- PQMarcDownload.py
+++ PQMarcDownload.py
@@ -2,12 +2,14 @@
 # -*- coding: UTF-8 -*-
 
 import requests
+import re
 
-year = str(raw_input('Please input the year for your query: '))
-wskey = str(raw_input('Please provide your wskey: '))
-print("")
-urlInput = str(raw_input('PQ1 = search.proquest.com; PQ2 = gateway.proquest.com; PQ3 = wwwlib.umi.com; or input a custom url: '))
+year = str(input('Please input the year for your query: '))
+wskey = str(input('Please provide your wskey: '))
+urlInput = str(input('PQ1 = search.proquest.com; PQ2 = gateway.proquest.com; PQ3 = wwwlib.umi.com; or input a custom url: '))
 beginPoint = 1
+beginpointMax = 10001
+loopCounter = 1
 
 if urlInput == "PQ1":
       queryUrl= "search.proquest.com/"
@@ -17,19 +19,26 @@ elif urlInput == "PQ3":
 	queryUrl = "wwwlib.umi.com/"
 else:
       queryUrl = urlInput
-
-while beginPoint <= 10001:
+	
+while beginPoint <= beginpointMax:
 	with open("PQDTMarcRecords" + year + ".txt", "a+") as f:
+		print("loop number: " + str(loopCounter) + " and max number of records: " + str(beginpointMax))
 #Creates and opens a text file and gives it the name PQDTMarcRecords and then the year given for the query (ex. PQDTMarcRecords2018)
 		print("New value of beginPoint is: " + str(beginPoint))
 		url = 'http://www.worldcat.org/webservices/catalog/search/worldcat/sru?query=srw.am%3D%22'+queryUrl+'%2a%22+and+srw.mt%3Ddeg+and+srw.mt%3Debk+and+srw.yr%3D'+year+'&wskey='+wskey+'&servicelevel=full&maximumRecords=100&startRecord=' + str(beginPoint)
-	#url variable includes the url that is being searched for, the material type for dissertation, the input year, the wskey that you MUST provide, and the variable for the beginning point for the page
- 		r = requests.get(url).text
- 		marcRecords = r.encode('utf-8')
- 		f.write(marcRecords)
-	#sends the request and writes the MARCXML to the file
- 		beginPoint += 100
+#url variable includes the url that is being searched for, the material type for dissertation, the input year, the wskey that you MUST provide, and the variable for the beginning point for the page
+		r = requests.get(url).text
+		records = r.encode('utf-8')
+		marcRecords = str(records)
+		beginPoint += 100
+		f.write(marcRecords)
+		if loopCounter == 1:
+			m = re.search(r'<numberOfRecords>(\d+)</numberOfRecords>', marcRecords)
+			beginpointMax = int(m.group(1))
+			loopCounter += 1
+		else:
+			loopCounter += 1
+			
  	#100 is added to the beginPoint, and the loop runs again
 else:
-	f.close()
 	print('done')
