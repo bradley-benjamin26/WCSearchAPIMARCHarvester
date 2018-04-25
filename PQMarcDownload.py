3fcb2680b09cae51b49f6e4477893c3e7b4ee2e9
Benjamin Bradley <bbradle1@umd.edu>
2018-04-25 14:32:51 -0400

    Update
    Script now reads the file to find the number of hits. The script original sent queries until it hit the 10,000 results cap (or the script was manually terminated), but now the script will only query and page through results up through the complete set of results. The script now can handle queries that exceed the API's 10,000 result limit by initiating iterative title searches (iterating through the alphabet) and if title-letter search still returns more than 10,000 results it will trigger an iterative author search. I have also added addition search parameters. These are focused on the dissertations project I'm using the script for, and long term, I hope to remove these and make the script's query options more flexible.

--- PQMarcDownload.py
+++ PQMarcDownload.py
@@ -3,13 +3,34 @@
 
 import requests
 import re
+import string
 
-year = str(input('Please input the year for your query: '))
-wskey = str(input('Please provide your wskey: '))
-urlInput = str(input('PQ1 = search.proquest.com; PQ2 = gateway.proquest.com; PQ3 = wwwlib.umi.com; or input a custom url: '))
-beginPoint = 1
-beginpointMax = 10001
-loopCounter = 1
+year = str(raw_input('Please input the year for your query: '))
+wskey = str(raw_input('Please provide your wskey: '))
+urlInput = str(raw_input('PQ1 = search.proquest.com; PQ2 = gateway.proquest.com; PQ3 = wwwlib.umi.com; or input a custom url: '))
+ebkCheck = raw_input("Check for ebooks?(yes/no): ")
+langCheck = raw_input("Checked for English items only?: ")
+degChecker = raw_input("search for dissertations? ")
+title = str(raw_input("File name for output: "))
+alpha = list(string.ascii_uppercase)
+max = ''
+
+
+
+if ebkCheck == 'yes':
+	ebk = '+and+srw.mt%3Dweb'
+else:
+	ebk = ''
+
+if langCheck == 'yes':
+	lang = '+and+srw.la%3Deng'
+else:
+	lang = ''
+
+if degChecker == 'yes':
+	deg = '+and+srw.mt%3Ddeg'
+else:
+	deg = ''
 
 if urlInput == "PQ1":
       queryUrl= "search.proquest.com/"
@@ -19,26 +40,76 @@ elif urlInput == "PQ3":
 	queryUrl = "wwwlib.umi.com/"
 else:
       queryUrl = urlInput
+      
+url = 'http://www.worldcat.org/webservices/catalog/search/worldcat/sru?query=srw.am%3D%22'+queryUrl+'%2a%22'+deg+ebk+lang+'+and+srw.yr%3D'+year+'&wskey='+wskey+'&servicelevel=full'
 	
-while beginPoint <= beginpointMax:
-	with open("PQDTMarcRecords" + year + ".txt", "a+") as f:
-		print("loop number: " + str(loopCounter) + " and max number of records: " + str(beginpointMax))
-#Creates and opens a text file and gives it the name PQDTMarcRecords and then the year given for the query (ex. PQDTMarcRecords2018)
-		print("New value of beginPoint is: " + str(beginPoint))
-		url = 'http://www.worldcat.org/webservices/catalog/search/worldcat/sru?query=srw.am%3D%22'+queryUrl+'%2a%22+and+srw.mt%3Ddeg+and+srw.mt%3Debk+and+srw.yr%3D'+year+'&wskey='+wskey+'&servicelevel=full&maximumRecords=100&startRecord=' + str(beginPoint)
-#url variable includes the url that is being searched for, the material type for dissertation, the input year, the wskey that you MUST provide, and the variable for the beginning point for the page
-		r = requests.get(url).text
-		records = r.encode('utf-8')
-		marcRecords = str(records)
-		beginPoint += 100
-		f.write(marcRecords)
-		if loopCounter == 1:
-			m = re.search(r'<numberOfRecords>(\d+)</numberOfRecords>', marcRecords)
-			beginpointMax = int(m.group(1))
-			loopCounter += 1
+#sends initial request to find the number of results to determine course of action
+r = requests.get(url).text
+records = r.encode('utf-8')
+marcRecords = str(records)
+m = re.search(r'<numberOfRecords>(\d+)</numberOfRecords>', marcRecords)
+beginpointMax = int(m.group(1))
+
+#if the query returns more than 10,000 results which is greater than can be downloaded, the script performs a search through the alphabet for titles otherwise it will iterate through results by pages only
+
+if beginpointMax > 10000:
+	print('Results greater than 10,000. Performing title searches ' + str(beginpointMax))
+	with open(title + ".txt", "a+") as f:	
+		for letter in alpha:
+			url = 'http://www.worldcat.org/webservices/catalog/search/worldcat/sru?query=srw.am%3D%22'+queryUrl+'%2a%22+and+srw.ti%3D%22'+letter+'%22'+deg+ebk+lang+'+and+srw.yr%3D'+year+'&wskey='+wskey+'&servicelevel=full&maximumRecords=100&startRecord='
+			search = requests.get(url).text
+			results = search.encode('utf-8')									
+			data = str(results)
+			m = re.search(r'<numberOfRecords>(\d+)</numberOfRecords>', data)
+			max = int(m.group(1))
+			print("Performing author search, title search returned too many hits: ", max)
+			if max > 10000:
+				authorAlpha = alpha
+				for authorLetter in authorAlpha:
+					beginPoint = 1
+					while beginPoint <= max:
+						print(letter, authorLetter, beginPoint, max)
+						url = 'http://www.worldcat.org/webservices/catalog/search/worldcat/sru?query=srw.am%3D%22'+queryUrl+'%22+and+srw.ti%3D%22'+letter+'%22+and+srw.au%3D%22'+authorLetter+'%22'+deg+ebk+lang+'+and+srw.yr%3D'+year+'&wskey='+wskey+'&servicelevel=full&maximumRecords=100&startRecord=' + str(beginPoint)
+						search = requests.get(url).text
+						results = search.encode('utf-8')					
+						data = str(results)
+						beginPoint += 100
+						f.write(data)
+						m = re.search(r'<numberOfRecords>(\d+)</numberOfRecords>', data)
+						max = int(m.group(1))
+			else:
+				beginPoint = 1			
+				while beginPoint <= max + 1:
+					print(letter, beginPoint, max)
+					url = 'http://www.worldcat.org/webservices/catalog/search/worldcat/sru?query=srw.am%3D%22'+queryUrl+'%22+and+srw.ti%3D%22'+letter+'%22'+deg+ebk+lang+'+and+srw.yr%3D'+year+'&wskey='+wskey+'&servicelevel=full&maximumRecords=100&startRecord=' + str(beginPoint)
+					search = requests.get(url).text
+					results = search.encode('utf-8')					
+					data = str(results)
+					beginPoint += 100
+					f.write(data)
+					m = re.search(r'<numberOfRecords>(\d+)</numberOfRecords>', data)
+					max = int(m.group(1))
 		else:
-			loopCounter += 1
-			
- 	#100 is added to the beginPoint, and the loop runs again
-else:
-	print('done')
+			print('done')
+		
+else:	
+	beginPoint = 1
+	while beginPoint <= beginpointMax + 1:
+		with open(title + ".txt", "a+") as f:
+			print(beginPoint, beginpointMax)
+			print("New value of beginPoint is: " + str(beginPoint))
+			url = 'http://www.worldcat.org/webservices/catalog/search/worldcat/sru?query=srw.am%3D%22'+queryUrl+'%22'+deg+ebk+lang+'+and+srw.yr%3D'+year+'&wskey='+wskey+'&servicelevel=full&maximumRecords=100&startRecord=' + str(beginPoint)
+			r = requests.get(url).text
+			records = r.encode('utf-8')			
+			marcRecords = str(records)
+			beginPoint += 100
+			f.write(marcRecords)
+			if beginPoint == 1:
+				m = re.search(r'<numberOfRecords>(\d+)</numberOfRecords>', marcRecords)
+				beginpointMax = int(m.group(1))
+				beginPoint += 100
+				
+			else:
+				beginPoint += 100
+	else:
+		print('done')
